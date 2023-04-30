137f75fd810d0da541736fee2a7ad3e1fac1ad91
shendrongddv <shendrongddv@gmail.com>
2023-04-30 17:18:34 +0700

    initial

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,22 +3,21 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div className="mx-auto max-w-7xl px-4 py-12">
-      <div className="flex flex-col-reverse gap-10 md:flex-row md:items-center md:justify-between">
-        <div className="flex-1 border">
-          <div className="space-y-6 text-center md:text-start ">
-            <h1 className="text-[32px] font-bold leading-tight text-[#333333] md:text-[56px]">
-              Jasa Convert Pulsa yang Dipercaya Jutaan Customer Sejak 2010.
-            </h1>
-            <p className="text-[#666666]">
-              Punya Pulsa Berlebih Tapi Nggak Dipakai? Tukar Pulsa Kamu Jadi
-              Uang / Saldo eWallet hanya dalam hitungan menit di Viapulsa!
-            </p>
-            <Link to={"/"} className="w-auto rounded-full px-8 py-4 shadow">
-              Convert Pulsa Sekarang!
-            </Link>
-          </div>
+      <div className="flex flex-col-reverse items-center justify-center gap-8 md:flex-row md:items-center md:justify-between">
+        <div className="flex-1 text-center md:text-left">
+          <h1 className="text-[32px] font-bold leading-tight text-[#333333] md:text-[56px]">
+            Jasa Convert Pulsa yang Dipercaya Jutaan Customer Sejak 2010.
+          </h1>
+          <p className="mt-4 text-[#666666]">
+            Punya Pulsa Berlebih Tapi Nggak Dipakai? Tukar Pulsa Kamu Jadi Uang
+            / Saldo eWallet hanya dalam hitungan menit di Viapulsa!
+          </p>
+          <button className="btn btn-primary mt-4">
+            Convert Pulsa Sekarang!
+          </button>
         </div>
-        <div className="flex-1 border">
+
+        <div className="flex-1">
           <div className="flex items-center justify-center md:justify-end">
             <img className="w-[80%]" src="./hero1.png" alt="" />
           </div>
